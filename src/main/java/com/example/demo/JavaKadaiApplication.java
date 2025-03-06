297883c47df9896816224a9aee9d4567f595f2ca
LAPTOP-5P9L9CG2\river <takahashi.rei@compass-japan.com>
2025-03-06 11:14:41 +0900

    first commit

---
+++ src/main/java/com/example/demo/JavaKadaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKadaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKadaiApplication.class, args);
+	}
+
+}
