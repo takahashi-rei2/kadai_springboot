34ce179fd06175b0cf86f5b823002a67d627b248
LAPTOP-5P9L9CG2\river <takahashi.rei@compass-japan.com>
2025-03-28 17:34:45 +0900

    問題④回答（再送信）

--- src/test/java/com/example/demo/Looptest4.java
+++ src/test/java/com/example/demo/Looptest4.java
@@ -1,5 +1,43 @@
 package com.example.demo;
 
-public class Looptest4 {
+import org.junit.jupiter.api.Test;
 
+public class Looptest4 {
+	@Test
+	void mondai1() {      
+	    for (int i = 0; i < 5; i++) {
+	        for (int j = 1; j < 6; j++) {
+	            System.out.print(i*5+j+" "); 
+	        }
+	        System.out.println();
+	    }
+	}     
+	@Test
+    void mondai2() {      
+	    for (int i = 0; i < 5; i++) {
+	        for (int j = 1; j < 6; j++) {
+	            int N=i*5+j;
+	                if(N<10){
+	                    System.out.print(N+"  "); 
+	                }else{
+	                    System.out.print(N+" ");   
+	                }
+	        }
+	            System.out.println();
+	    }
+    }       
+    @Test
+    void mondai3() {      
+    	for (int i = 1; i < 6; i++) {
+            for (int j = 1; j < 6; j++) {
+                int N=i*j;
+                if(N<10){
+                    System.out.print(N+"  "); 
+                }else{
+                    System.out.print(N+" ");   
+                }
+            }
+                System.out.println();
+        }
+    }
 }
