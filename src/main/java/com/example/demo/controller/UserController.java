d469d17568d5f4f1e5fd70af2bc7fca87e06e459
LAPTOP-5P9L9CG2\river <takahashi.rei@compass-japan.com>
2025-04-01 16:52:42 +0900

    課題４製作いたしました

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,26 +1,43 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.User;
-
+import com.example.demo.repository.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.repository.UserRepository;
+import java.time.LocalDateTime;
 import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-	@Autowired
-    private  UserRepository userRepository;
+    @Autowired
+    private UserMapper userMapper;
 
-   
-
-    // データベースから取得したユーザーリストを返すエンドポイント
+    // 全ユーザーを取得
     @GetMapping
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return userMapper.findAll();
+    }
+
+    // 新規ユーザー追加
+    @PostMapping("/add")
+    public String addUser(@RequestBody User user) {
+        // 新規ユーザーの作成
+        user.setCreated(LocalDateTime.now());
+        user.setEnabled(true);
+        userMapper.insert(user);
+        return "User added successfully!";
+    }
+
+    // ユーザー情報を更新
+    @PutMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") int id, @RequestBody User user) {
+        // 更新するユーザーのIDを設定
+        user.setId(id);
+        user.setLastLogined(LocalDateTime.now());
+        userMapper.update(id, user);
+        return "User updated successfully!";
     }
 }
--- src/main/java/com/example/demo/repository/mapper/UserMapper.java
+++ src/main/java/com/example/demo/repository/mapper/UserMapper.java
@@ -1,17 +1,20 @@
 package com.example.demo.repository.mapper;
 
 import com.example.demo.model.User;
-import org.apache.ibatis.annotations.Mapper;
-
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface UserMapper {
 
-    
-    public int count();
+    int count();
 
-    
-    List<User> findAll();  // 修正・確認
+    List<User> findAll();
+
+    int insert(User user);
+
+    int update(@Param("id") int id, @Param("user") User user);
+
+    User findById(@Param("id") int id);
 }
